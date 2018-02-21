874a595fe314ad22bac0ed6f82ea96a0c055a315
rayjune <ray_june@qq.com>
2018-02-21 14:30:05 +0800

    explore JSX: one root, one love :)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,45 @@
 console.log('App is runing');
 
 // JSX JavaScript XML
+// error: var templete = <h1>This is JSX from app.js hahaha</h1><p>This is some info</p>;
+// one root element(wrapper div), one love :)
 var templete = React.createElement(
-  'h1',
+  'div',
   null,
-  'This is JSX from app.js hahaha'
+  React.createElement(
+    'h1',
+    null,
+    'Another RayJune'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'motto: Gentleman is gentle.'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Hobby(except coding)'
+  ),
+  React.createElement(
+    'ul',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Football'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Man UTD'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Zlatan 10'
+    )
+  )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 console.log('App is runing');
 
 // JSX JavaScript XML
-var templete = <h1>This is JSX from app.js hahaha</h1>;
+// error: var templete = <h1>This is JSX from app.js hahaha</h1><p>This is some info</p>;
+// one root element(wrapper div), one love :)
+var templete = (
+  <div>
+    <h1>Another RayJune</h1>
+    <p>motto: Gentleman is gentle.</p>
+    <p>Hobby(except coding)</p>
+    <ul>
+      <li>Football</li>
+      <li>Man UTD</li>
+      <li>Zlatan 10</li>
+    </ul>
+  </div>
+)
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(templete, appRoot);
