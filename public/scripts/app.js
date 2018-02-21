d26209f02f70f5bd7e8dd84a5e770810e2c18f28
rayjune <ray_june@qq.com>
2018-02-21 11:00:27 +0800

    config yarn & babel => npm scripts, build fundamental workflow

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
+'use strict';
+
 console.log('App is runing');
 
 // JSX JavaScript XML
-// var templete = <p>This is JSX from app.js</p>;
-// compilerd from https://babeljs.io/repl/
 var templete = React.createElement(
-  "h1",
-  { id: "someid" },
-  "Hola, RayJune"
+  'h1',
+  null,
+  'This is JSX from app.js hahaha'
 );
 var appRoot = document.getElementById('app');
 
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App is runing');
+
+// JSX JavaScript XML
+var templete = <h1>This is JSX from app.js hahaha</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templete, appRoot);
