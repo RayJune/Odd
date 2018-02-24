072ccddbf59502379c8d34e6aa0c2148e8638d16
rayjune <ray_june@qq.com>
2018-02-24 09:11:52 +0800

    compare two ways to insert JSX into a component(children VS props)

--- src/app.js
+++ src/app.js
@@ -2,55 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import OddApp from './components/OddApp';
 
-ReactDOM.render(<OddApp />, document.getElementById('app'));
-
-class OldSyntax {
-  constructor() {
-    this.name = 'ray';
-  }
-  getGreeting() {
-    return `hi~ my name is ${this.name}`;
-  }
+const Layout1 = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.template}
+      <p>footer</p>
+    </div>
+  );
 }
 
-const num1 = new OldSyntax();
-console.log(num1);
-console.log(num1.getGreeting());
-// const getGreeting = num1.getGreeting;
-// console.log(getGreeting());
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+)
 
-// fix:
+// ReactDOM.render(<Layout1 template={template}/>, document.getElementById('app'));
 
-class OldSyntax2 {
-  constructor() {
-    this.name = 'ray';
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `hi~ my name is ${this.name}`;
-  }
+const Layout2 = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  );
 }
 
-const num11 = new OldSyntax2();
-console.log(num11.getGreeting());
-const getGreeting2 = num11.getGreeting;
-console.log(getGreeting2());
+// ReactDOM.render(<Layout2><p>inline</p></Layout2>, document.getElementById('app'));
 
-class NewSyntax {
-  name = 'liga';
-}
-
-const num2 = new NewSyntax();
-console.log(num2);
-
-class NewSyntax2 {
-  name = 'liga';
-  getGreeting = () => {
-    return `hi~ my name is ${this.name}`;
-  }
-}
-
-const num22 = new NewSyntax2();
-console.log(num22.getGreeting());
-const getGreeting22 = num22.getGreeting;
-console.log(getGreeting22());
+ReactDOM.render((
+  <Layout2>
+    <p>inline</p>
+  </Layout2>
+), document.getElementById('app'));
