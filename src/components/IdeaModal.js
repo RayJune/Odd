e935061a26509e3a88b76dd185fb319568d2c792
rayjune <ray_june@qq.com>
2018-02-24 22:14:39 +0800

    add style to thrid-party component, and adjust other little style

--- src/components/IdeaModal.js
+++ src/components/IdeaModal.js
@@ -6,9 +6,12 @@ const IdeaModal = (props) => (
     isOpen={!!props.selectedidea}
     onRequestClose={props.handlerClearSelectedidea}
     contentLabel="Selected idea"
+    closeTimeoutMS={200}
+    className="modal"
   >
-    {props.selectedidea && <h3>{props.selectedidea}</h3>}
-    <button onClick={props.handlerClearSelectedidea}>Okay</button>
+    <h3 className="modal__title">Puncnline</h3>
+    {props.selectedidea && <p className="modal__body">{props.selectedidea}</p>}
+    <button className="button" onClick={props.handlerClearSelectedidea}>了然</button>
   </Modal>
 );
 
