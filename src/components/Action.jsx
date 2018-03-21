363b88fd8fc57273ed1b91956c62c0b9bcdef051
rayjune <ray_june@qq.com>
2018-03-21 14:02:51 +0800

    adjust style and some other little details

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const Action = props => (
-  <div>
-    <button
-      className="button big-button"
-      onClick={props.handlePick}
-      disabled={!props.hasPoems}
-    >
-        点亮一首诗
-    </button>
-  </div>
+  <button
+    className="button big-button"
+    onClick={props.handlePick}
+    disabled={!props.hasPoems}
+  >
+    点亮一首诗
+  </button>
 );
 
 export default Action;
